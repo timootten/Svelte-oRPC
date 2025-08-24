95ca89167c102bf7def8a68811afa1ca1acb9712
Timo Otten <timootten@icloud.com>
2025-08-24 18:24:41 +0200

    Add Timer component for live time display; implement timer functionality and integrate with orpc

---
+++ src/components/Timer.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import { live } from '$lib/client/live.svelte';
+	import { orpc } from '$lib/client/orpc';
+
+	const { abc } = $props<{ abc: string }>();
+
+	let timer = live(orpc.timer.live());
+</script>
+
+<h2>Live Timer</h2>
+{#if timer.current}
+	<p>Current time: {timer.current}</p>
+{:else}
+	<p>Loading...</p>
+{/if}
+
+<h1>{abc}</h1>
---
+++ src/routes/test/+page.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import { orpc } from '$lib/client/orpc';
+	import Timer from '../../components/Timer.svelte';
+	import { live } from '../../lib/client/live.svelte';
+
+	let timer = live(orpc.timer.live());
+
+	const abc = 'sdsdsd';
+</script>
+
+<h2>Live Timer</h2>
+{#if timer.current}
+	<p>Current time: {timer.current}</p>
+{:else}
+	<p>Loading...</p>
+{/if}
+
+-------------------
+
+<Timer {abc} />
