83692b075d6bb2a38e4dcd780b9edbcfad1fc9ab
Timo Otten <timootten@icloud.com>
2025-08-28 03:05:44 +0200

    feat: implement new page server logic for planet queries and add dependency handling

--- src/routes/tsquery/planets/new/+page.server.ts
+++ src/routes/tsquery/planets/new/+page.server.ts
@@ -1,6 +1,9 @@
 import { query } from "$lib/client/orpc"
 
-export const load = async ({ isDataRequest }) => {
+export const load = async ({ depends, isDataRequest }) => {
+
+  depends("tsquery:planets");
+
   return {
     list: isDataRequest ? undefined : await query.planet.list()
   }
